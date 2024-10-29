bb1f62c3e73752a083de064ac945a2b21852c7db
Cindy Jiang <ziyijian@andrew.cmu.edu>
2024-10-29 17:54:36 -0400

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Cindy Jiang on 2024/10/29.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
