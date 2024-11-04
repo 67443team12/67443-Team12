bcdcb6530afa86fc1b98811a4f86fc8140277460
Kailan Mao <kailanm@andrew.cmu.edu>
2024-11-03 22:50:38 -0500

    replace with mine

---
+++ kailan-team12/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  kailan_team12App.swift
+//  kailan-team12
+//
+//  Created by k mao on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
